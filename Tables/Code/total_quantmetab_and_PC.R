a148d007144bd27899dc948c32113f53c556c5d2
KHeal <kheal@uw.edu>
2020-07-24 12:57:56 -0700

    fixed PC numbers on tables

--- Tables/Code/total_quantmetab_and_PC.R
+++ Tables/Code/total_quantmetab_and_PC.R
@@ -1,11 +1,4 @@
-# %TO DO: make supplemental table with total quantified compared to PC PN, nmole C and N average SD for metabolites, nm ave SD for PC, PN
-# \begin{table}[ht]
-# \centering
-# \caption{\label{Quan_withPC} Total quantified compared to PC, with the nmole average, sd, for both metabolites and PC with propogated error.  Also should have nmolN and errors} 
-# \end{table}
-
-#TO DO: make all the (NA)s go away
-
+#TO DO: calculate errors for MGL and KM samples
 library(tidyverse)
 library(fuzzyjoin)
 
@@ -18,6 +11,8 @@ meta.dat.filename <- "MetaData/SampInfo_wMetaData_withUTC.csv"
 dat <- read_csv(dat.filename)
 dat.pc <- read_csv(PC.dat.filename)
 meta.dat <- read_csv(meta.dat.filename)%>% select(SampID, Station_1, Cruise, latitude, longitude, Depth)
+meta.dat.wPC <- read_csv(meta.dat.filename)%>% 
+  select(Station_1, Cruise, latitude, longitude, Depth, PC_ave, PC_Stdev, PN_ave, PN_Stdev) %>% unique()
 
 #Clean it up
 dat.clean <- dat %>%
@@ -45,37 +40,54 @@ PC.dat.summary <- dat.pc %>%
             latitude = mean(Latitude))
 
 mesh.KOK.PC <- dat.clean %>%
+  filter(Cruise == "KOK1606") %>%
   difference_left_join(PC.dat.summary, by = "latitude", max_dist = 0.5,
                        distance_col = "lat.difference") %>%
   filter(Station_1 != 7 | lat.difference < 0.47) %>%
-  filter(Station_1 != 8 | lat.difference < 0.46) %>%
-  mutate(nmolave.PC = ifelse(Cruise == "KM1513", NA, nmolave.PC), 
-         nmolave.PN = ifelse(Cruise == "KM1513", NA, nmolave.PN),
-         nmolstd.PC = ifelse(Cruise == "KM1513", NA, nmolstd.PC),
-         nmolstd.PN = ifelse(Cruise == "KM1513", NA, nmolstd.PN))%>%
- 
-    mutate(fraction.PC = nmolave.metabC/nmolave.PC) %>%
-  mutate(fraction.PC.sd = ifelse(is.na(nmolstd.metabC), fraction.PC*(nmolstd.PC/nmolave.PC),
-                                 fraction.PC*(nmolstd.metabC/nmolave.PC+nmolstd.PC/nmolave.PC))) %>%
+  filter(Station_1 != 8 | lat.difference < 0.46) 
+
+mesh.nonKOK.PC <- dat.clean %>%
+  filter(Cruise != "KOK1606") %>%
+  left_join(meta.dat.wPC, by = c("Depth", "Cruise"))%>%
+  mutate(nmolave.PC = mean(PC_ave)*1000,
+            nmolstd.PC = sd(PC_Stdev)*1000, 
+            nmolave.PN = mean(PN_ave)*1000,
+            nmolstd.PN = sd(PN_Stdev)*1000)
+
+mesh.KOK.and.nonKOK <- mesh.KOK.PC %>%
+  bind_rows(mesh.nonKOK.PC) %>%
+  mutate(fraction.PC = nmolave.metabC/nmolave.PC) %>%
+  mutate(fraction.PC.sd = ifelse(is.na(nmolstd.metabC), 
+                                 fraction.PC*(nmolstd.PC/nmolave.PC),
+                                 ifelse(is.na(nmolstd.PC),
+                                        fraction.PC*(nmolstd.metabC/nmolave.PC),
+                                 fraction.PC*(nmolstd.metabC/nmolave.PC+nmolstd.PC/nmolave.PC)))) %>%
   mutate(fraction.PN = nmolave.metabN/nmolave.PN) %>%
-  mutate(fraction.PN.sd = ifelse(is.na(nmolstd.metabN), fraction.PN*(nmolstd.PN/nmolave.PN),
-                                 fraction.PN*(nmolstd.metabN/nmolave.PN+nmolstd.PN/nmolave.PN))) 
+  mutate(fraction.PN.sd = ifelse(is.na(nmolstd.metabN), 
+                                 fraction.PN*(nmolstd.PN/nmolave.PN),
+                                 ifelse(is.na(nmolstd.PN),
+                                        fraction.PC*(nmolstd.metabN/nmolave.PN),
+                                 fraction.PN*(nmolstd.metabN/nmolave.PN+nmolstd.PN/nmolave.PN)))) 
 
 
-mesh.KOK.PC.new.names <- mesh.KOK.PC %>%
+mesh.KOK.PC.new.names <- mesh.KOK.and.nonKOK %>%
   ungroup() %>%
   mutate(nmolave.metabC.withstd = ifelse(is.na(nmolstd.metabC), as.character(round(nmolave.metabC, digits = 1)) , 
                                          paste0(round(nmolave.metabC, digits = 1), " (", round(nmolstd.metabC, digits = 1), ")"))) %>%
   mutate(nmolave.PC.withstd = ifelse(is.na(nmolave.PC), NA,
-           paste0(round(nmolave.PC, digits = -1), " (", round(nmolstd.PC, digits = -1), ")")) ) %>%
+                                     ifelse(is.na(nmolstd.PC),  paste0(round(nmolave.PC, digits = -1)),
+                                     paste0(round(nmolave.PC, digits = -1), " (", round(nmolstd.PC, digits = -1), ")")))) %>%
   mutate(percent.PC.withstd = ifelse(is.na(nmolave.PC), NA,
-           paste0(round(fraction.PC*100, digits = 1), " (", round(fraction.PC.sd*100, digits = 1), ")"))) %>%
+                                     ifelse(is.na(nmolstd.PC), paste0(round(fraction.PC*100, digits = 1)),
+                                     paste0(round(fraction.PC*100, digits = 1), " (", round(fraction.PC.sd*100, digits = 1), ")")))) %>%
   mutate(nmolave.metabN.withstd = ifelse(is.na(nmolstd.metabN), as.character(round(nmolave.metabN, digits = 1)) , 
-                                         paste0(round(nmolave.metabN, digits = 1), " (", round(nmolstd.metabN, digits = 1), ")"))) %>%
+                                         paste0(round(nmolave.metabN, digits = 1), " (", round(nmolstd.metabN, digits = 2), ")"))) %>%
   mutate(nmolave.PN.withstd = ifelse(is.na(nmolave.PN), NA,
-                                     paste0(round(nmolave.PN, digits = -1), " (", round(nmolstd.PN, digits = -1), ")")) ) %>%
+                                     ifelse(is.na(nmolstd.PN),paste0(round(nmolave.PN, digits = -1)),
+                                     paste0(round(nmolave.PN, digits = -1), " (", round(nmolstd.PN, digits = -1), ")")) ))%>%
   mutate(percent.PN.withstd = ifelse(is.na(nmolave.PN), NA,
-                                     paste0(round(fraction.PN*100, digits = 1), " (", round(fraction.PN.sd*100, digits = 1), ")"))) %>%
+                                     ifelse(is.na(nmolstd.PN),paste0(round(fraction.PN*100, digits = 1)),
+                                     paste0(round(fraction.PN*100, digits = 1), " (", round(fraction.PN.sd*100, digits = 2), ")")))) %>%
   select(Cruise, latitude.x, Depth,  
          nmolave.metabC.withstd, nmolave.PC.withstd, percent.PC.withstd, nmolave.metabN.withstd, nmolave.PN.withstd, percent.PN.withstd) %>%
   rename(`Total measured meatbolites (nmol C L-1)` = nmolave.metabC.withstd, 
